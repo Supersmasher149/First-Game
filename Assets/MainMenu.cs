e25621dae49d062a761c2a11702601c89828af33
Supersmasher149 <walter.gibbons@gmail.com>
2021-01-11 11:37:07 -0500

    init

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private void PlayGame()
+    {
+        SceneManager.LoadScene("New Scene");
+    }
+
+    private void QuitGame()
+    {
+        Debug.Log("QuitGame");
+        Application.Quit();
+    }
+}
---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Pause();
+            }
+        }
+    }
+
+    private void Resume()
+        {
+            pauseMenuUI.SetActive(false);
+            Time.timeScale = 1;
+            gameIsPaused = false;
+        }
+
+    private void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        gameIsPaused = true;
+    }
+
+    private void Quit()
+    {
+        Application.Quit();
+    }
+
+    private void Menu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
+    
